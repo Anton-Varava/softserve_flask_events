9f432f1df69db01dea71acb5a3d2c8778935b05f
Anton-Varava <varava.anton@gmail.com>
2021-09-15 23:30:11 +0300

    Refactored authentication.

--- resources/authentocation.py
+++ resources/authentocation.py
@@ -1,9 +1,10 @@
 import jwt
 
-from flask import request, make_response, jsonify
+from flask import request, make_response, jsonify, abort
 
 from app import app
 from models.user import User
+
 
 def token_required(func):
     def decorated(*args, **kwargs):
@@ -13,12 +14,13 @@ def token_required(func):
             token = request.headers['Authorization']
 
         if not token:
-            return make_response(jsonify({'message': 'Token is missing!'}), 401)
+            return make_response(jsonify({'message': 'Token is missing.'}), 400)
         try:
-            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-            current_user = User.query.filter_by(id=data['id']).first()
+            token_data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
         except:
-            return make_response(jsonify({'message': 'Token is invalid!'}), 401)
+            return make_response(jsonify({'message': 'Token is invalid.'}), 401)
+
+        current_user = User.query.filter_by(id=token_data['id']).first()
         return func(current_user=current_user, *args, **kwargs)
 
     return decorated
