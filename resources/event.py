92a1a52456ce4c034e0528e6d3e1a13a234eb0ac
Anton-Varava <varava.anton@gmail.com>
2021-09-21 15:57:24 +0300

    Edit events resources.

--- resources/event.py
+++ resources/event.py
@@ -1,14 +1,21 @@
 import requests
 
+from sqlalchemy.orm import joinedload
+
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
 from marshmallow import ValidationError
 
-from flask_restful import Resource, abort, reqparse
+from flask_restful import Resource, abort, reqparse, marshal_with
+
+from flask_apispec.views import MethodResource
 
 from flask import request, make_response, jsonify
 
 from app import db
 from schemas.event import event_schema, event_list_schema
 from models.event import Event, EventInvitedGuest
+from models.user import User
 from models.api_source import APISource
 from .authentication import token_required
 
@@ -19,8 +26,10 @@ events_post_args.add_argument('date', type=str, help='Date of the event in forma
 events_post_args.add_argument('description', type=str, help='Description of the event')
 
 
-class EventsListAPIView(Resource):
-    @token_required
+
+
+class EventsListAPIView(MethodResource, Resource):
+    @jwt_required()
     def get(self, *args, **kwargs):
         query_params = request.args
 
@@ -40,17 +49,16 @@ class EventsListAPIView(Resource):
             abort(404, message='Required event not found.')
         return event_list_schema.dump(events)
 
-    @token_required
+    @jwt_required()
     def post(self, *args, **kwargs):
         json_data = events_post_args.parse_args()
         try:
-            data = event_schema.load(json_data)
+            new_event = event_schema.load(json_data)
         except ValidationError as err:
             return err.messages, 400
 
-        current_user = kwargs.get('current_user')
-        new_event = Event(title=data.get('title'), date=data.get('date'), description=data.get('description'),
-                          organizer_id=current_user.id)
+        current_user = User.query.get_or_404(get_jwt_identity())
+        new_event.organizer_id = current_user.id
         try:
             db.session.add(new_event)
             db.session.commit()
@@ -60,16 +68,18 @@ class EventsListAPIView(Resource):
 
 
 class EventDetailAPIView(Resource):
-    @token_required
+    @jwt_required()
     def get(self, *args, **kwargs):
         event = Event.query.get_or_404(kwargs.get('event_id'), description='Event does not exist or has been deleted.')
+        if event.is_draft:
+            abort(404, message='Event does not exist or has been deleted.')
         return event_schema.dump(event), 200
 
-    @token_required
+    @jwt_required()
     def patch(self, *args, **kwargs):
         event = Event.query.get_or_404(kwargs.get('event_id'), description='Event does not exist or has been deleted.')
 
-        if event.organizer_id != kwargs.get('current_user').id:
+        if event.organizer_id != get_jwt_identity():
             abort(403, message='You don\'t have a permissions to do this.')
 
         try:
@@ -97,10 +107,10 @@ class EventDetailAPIView(Resource):
             abort(409, message='Data is not valid.')
         return event_schema.dump(event), 200
 
-    @token_required
+    @jwt_required()
     def delete(self, *args, **kwargs):
         event = Event.query.get_or_404(kwargs.get('event_id'), description='Event does not exist or has been deleted.')
-        if event.organizer_id != kwargs.get('current_user').id:
+        if event.organizer_id != get_jwt_identity():
             abort(403, message='You don\'t have a permissions to do this.')
         db.session.delete(event)
         db.session.commit()
